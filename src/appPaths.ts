af84ec2c0084a6cefc93a8d226d84252b4bfdc97
Kevin Ghadyani <Saturn2888@gmail.com>
2023-12-01 03:29:09 -0600

    Upgrades MediaInfo

--- src/appPaths.ts
+++ src/appPaths.ts
@@ -6,6 +6,6 @@ export const ffmpegPath = (
   )
 )
 
-export const mediaInfoPath = "MediaInfo_CLI_23.07_Windows_x64/MediaInfo.exe"
+export const mediaInfoPath = "MediaInfo_CLI_23.11.1_Windows_x64/MediaInfo.exe"
 export const mkvMergePath = "mkvtoolnix-64-bit-78.0.7/mkvmerge.exe"
 export const mkvPropEditPath = "mkvtoolnix-64-bit-78.0.7/mkvpropedit.exe"
