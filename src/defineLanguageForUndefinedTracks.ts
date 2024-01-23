76a7232bd21f188cdbefdedf80449f40e8778430
Kevin Ghadyani <Saturn2888@gmail.com>
2024-01-23 05:34:09 -0600

    Cleans up code in defineLanguageForUndefinedTracks

--- src/defineLanguageForUndefinedTracks.ts
+++ src/defineLanguageForUndefinedTracks.ts
@@ -2,21 +2,23 @@ import {
   concatMap,
   filter,
   from,
-} from "rxjs";
+} from "rxjs"
 
+import {
+  getMkvInfo,
+  type MkvTookNixTrackType,
+} from "./getMkvInfo.js"
 import { type Iso6392LanguageCode } from "./iso6392LanguageCodes.js"
-import { getMkvInfo } from "./getMkvInfo.js";
-import { runMkvPropEdit } from "./runMkvPropEdit.js";
-import { type MkvTookNixTrackType } from "./getMkvInfo.js";
+import { runMkvPropEdit } from "./runMkvPropEdit.js"
 
 export const defineLanguageForUndefinedTracks = ({
   filePath,
   subtitleLanguage,
   trackType,
 }: {
-  filePath: string,
-  subtitleLanguage: Iso6392LanguageCode,
-  trackType: MkvTookNixTrackType,
+  filePath: string
+  subtitleLanguage: Iso6392LanguageCode
+  trackType: MkvTookNixTrackType
 }) => (
   getMkvInfo(
     filePath,
