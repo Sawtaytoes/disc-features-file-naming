a1f9de27a29409549c85cdcf3e76b83663b53cc6
Kevin Ghadyani <Saturn2888@gmail.com>
2024-03-31 01:37:06 -0500

    Upgrades to MkvToolNix 83.0

--- src/appPaths.ts
+++ src/appPaths.ts
@@ -7,6 +7,6 @@ export const ffmpegPath = (
 )
 
 export const mediaInfoPath = "MediaInfo_CLI_24.03_Windows_x64/MediaInfo.exe"
-export const mkvExtractPath = "mkvtoolnix-64-bit-78.0.7/mkvextract.exe"
-export const mkvMergePath = "mkvtoolnix-64-bit-78.0.7/mkvmerge.exe"
-export const mkvPropEditPath = "mkvtoolnix-64-bit-78.0.7/mkvpropedit.exe"
+export const mkvExtractPath = "mkvtoolnix-64-bit-83.0/mkvextract.exe"
+export const mkvMergePath = "mkvtoolnix-64-bit-83.0/mkvmerge.exe"
+export const mkvPropEditPath = "mkvtoolnix-64-bit-83.0/mkvpropedit.exe"
