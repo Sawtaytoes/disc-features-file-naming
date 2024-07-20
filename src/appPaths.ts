3b518496756b9e656fd3ca300c00eab2bd88419a
Kevin Ghadyani <Saturn2888@gmail.com>
2024-07-19 20:32:24 -0500

    Upgrades MediaInfo to 24.05

--- src/appPaths.ts
+++ src/appPaths.ts
@@ -14,7 +14,7 @@ export const topazFfmpeg = {
   ffmpegPath: "C:\\Program Files\\Topaz Labs LLC\\Topaz Video AI\\ffmpeg.exe",
 }
 
-export const mediaInfoPath = "MediaInfo_CLI_24.03_Windows_x64/MediaInfo.exe"
+export const mediaInfoPath = "MediaInfo_CLI_24.05_Windows_x64/MediaInfo.exe"
 export const mkvExtractPath = "mkvtoolnix-64-bit-84.0/mkvextract.exe"
 export const mkvMergePath = "mkvtoolnix-64-bit-84.0/mkvmerge.exe"
 export const mkvPropEditPath = "mkvtoolnix-64-bit-84.0/mkvpropedit.exe"
