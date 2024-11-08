0ddcccde5ca4665489d4b911ad9f5abd1aa01d37
Kevin Ghadyani <Saturn2888@gmail.com>
2024-11-08 00:40:36 -0600

    Upgrades MediaInfo to 24.11

--- src/appPaths.ts
+++ src/appPaths.ts
@@ -12,7 +12,7 @@ export const audioOffsetFinderPath = "audio-offset-finder"
 
 export const ffmpegPath = "apps.generated/ffmpeg-7.0.2-essentials_build/bin/ffmpeg.exe"
 
-export const mediaInfoPath = "apps.generated/MediaInfo_CLI_24.05_Windows_x64/MediaInfo.exe"
+export const mediaInfoPath = "apps.generated/MediaInfo_CLI_24.11_Windows_x64/MediaInfo.exe"
 
 export const mkvExtractPath = "apps.generated/mkvtoolnix-64-bit-88.0/mkvextract.exe"
 export const mkvMergePath = "apps.generated/mkvtoolnix-64-bit-88.0/mkvmerge.exe"
